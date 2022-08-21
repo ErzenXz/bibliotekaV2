701a9009dd78a09f344bff46d7dcaaf4497e995e
ErzenXs <njnana2017@gmail.com>
2022-08-21 12:39:10 +0200

    Fixed a bug that caused comments to not work correctly.

--- js/info.js
+++ js/info.js
@@ -1,8 +1,8 @@
 // Programoi Erzen Krasniqi
 
-// Librat dhe te dhënat e tyre Ardonis Byqmeti
+// Check if the user is logged 
 
-// Check if the user is logged in
+
 firebase.auth().onAuthStateChanged(function (e) { e ? (email = e.email, uid = e.uid) : document.getElementById("logout-id").style.display = "none" });
 
 function capitalizeFirstLetter(string) {
